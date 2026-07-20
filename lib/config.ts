1f23ed88d9ff06f780c0d1ec12bcd20da301b5c8
veloria04brand-cell <veloria04brand@gmail.com>
2026-07-20 21:04:20 +0100

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -4,9 +4,8 @@
 export const SHOP = {
   name: "VELORIA",
   tagline: "Accessoires premium, intemporels.",
-  whatsappNumber: "0708962553", // ⚠️ à remplacer par le vrai numéro du vendeur
+  whatsappNumber: "212708962553", // ⚠️ à remplacer par le vrai numéro du vendeur
   email: "contact@veloria-store.com",
-  address: "123 Avenue Mohammed V, Casablanca, Maroc",
-  instagram: "https://instagram.com/veloria",
-  facebook: "https://facebook.com/veloria",
+  instagram: "veloria04brand@gmail.com",
+  facebook: "https://www.facebook.com/profile.php?id=61589745449192",
 };
